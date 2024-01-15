fff0ae2bfe7b3ca9f94e2e34f995ba849bc27de8
wx-huang72 <83077840+wx-huang72@users.noreply.github.com>
2024-01-15 17:17:53 +0000

    Create 1907. Count Salary Categories.sql

---
+++ 1907. Count Salary Categories.sql
@@ -0,0 +1,81 @@
+"""
+  Table: Accounts
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| account_id  | int  |
+| income      | int  |
++-------------+------+
+account_id is the primary key (column with unique values) for this table.
+Each row contains information about the monthly income for one bank account.
+ 
+
+Write a solution to calculate the number of bank accounts for each salary category. The salary categories are:
+
+"Low Salary": All the salaries strictly less than $20000.
+"Average Salary": All the salaries in the inclusive range [$20000, $50000].
+"High Salary": All the salaries strictly greater than $50000.
+The result table must contain all three categories. If there are no accounts in a category, return 0.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Accounts table:
++------------+--------+
+| account_id | income |
++------------+--------+
+| 3          | 108939 |
+| 2          | 12747  |
+| 8          | 87709  |
+| 6          | 91796  |
++------------+--------+
+Output: 
++----------------+----------------+
+| category       | accounts_count |
++----------------+----------------+
+| Low Salary     | 1              |
+| Average Salary | 0              |
+| High Salary    | 3              |
++----------------+----------------+
+Explanation: 
+Low Salary: Account 2.
+Average Salary: No accounts.
+High Salary: Accounts 3, 6, and 8.
+  """
+
+# Write your MySQL query statement below
+
+# select category, count(category) as accounts_count 
+# from (
+#     select *, if(income>50000, "High Salary", if(income<20000, "Low Salary", "Average Salary")) as category
+#     from Accounts
+# ) as b
+# group by category
+
+# select "Low Salary" as category, ifnull(sum(income<20000),0) as accounts_count
+# from Accounts
+# union all 
+# select "High Salary" as category, ifnull(sum(income>50000),0) as accounts_count
+# from Accounts
+# union all
+# select "Average Salary" as category, ifnull(sum(income>=20000 and income <= 50000),0) as accounts_count
+# from Accounts
+
+(select 
+"Low Salary" as category,
+(select count(*)  from Accounts where income < 20000) as accounts_count)
+union all
+(select 
+"Average Salary" as category,
+(select count(*) from Accounts where income >= 20000 and income <= 50000) as accounts_count)
+union all 
+(select 
+"High Salary" as category,
+(select count(*) from Accounts where income > 50000) as accounts_count)
