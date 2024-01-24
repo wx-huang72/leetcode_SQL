aa7dbc25210c8bf388fd80cfcd084cb60414a2f0
wx-huang72 <83077840+wx-huang72@users.noreply.github.com>
2024-01-24 15:43:25 +0000

    Create 1084. Sales Analysis III.sql

---
+++ 1084. Sales Analysis III.sql
@@ -0,0 +1,38 @@
+"""
+  Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table can have duplicate rows.
+product_id is a foreign key (reference column) to the Product table.
+Each row of this table contains some information about one sale.
+ 
+  """
+
+# Write your MySQL query statement below
+select product_id, product_name
+from Product
+where product_id not in (
+    select product_id
+    from Sales
+    where sale_date not between "2019-01-01" and "2019-03-31"
+) and product_id in (select product_id from Sales)
